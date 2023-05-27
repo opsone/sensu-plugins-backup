120589eab3f47ca00270b63ee53f2eb79dce5c1b
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2023-05-27 18:11:34 +0200

    Fresh gem

---
+++ lib/sensu/plugins/backup.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'backup/version'
+
+module Sensu
+  module Plugins
+    module Backup
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
