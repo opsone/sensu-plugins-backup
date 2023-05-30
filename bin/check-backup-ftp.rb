0d4c75da50b7d9c3f0f9b0183379216d68abc7ab
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2023-05-30 13:33:04 +0200

    Add bins

---
+++ bin/check-backup-ftp.rb
@@ -0,0 +1,60 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: false
+
+require 'net/ftp'
+require 'sensu-plugin/check/cli'
+
+class CheckBackupFtp < Sensu::Plugin::Check::CLI
+  option :ftp_user,
+         short: '-u USER',
+         long: '--ftp-user USER',
+         description: 'FTP User'
+
+  option :ftp_password,
+         short: '-p PASS',
+         long: '--ftp-password PASS',
+         description: 'FTP Password'
+
+  option :ftp_host,
+         short: '-h HOST',
+         long: '--ftp-host HOST',
+         description: 'FTP Hostname'.dup,
+         required: true
+
+  option :ftp_directory,
+         short: '-D DIR_NAME',
+         long: '--ftp-directory DIR_NAME',
+         description: 'FTP directory'
+
+  option :directory_name,
+         short: '-d DIR_NAME',
+         long: '--directory-name DIR_NAME',
+         description: 'The name of directory to check',
+         default: '/var/archives'
+
+  def run
+    ftp = Net::FTP.new(config[:ftp_host])
+    ftp.login(config[:ftp_user], config[:ftp_password]) if config[:ftp_user] && config[:ftp_password]
+    ftp.chdir(config[:ftp_directory]) if config[:ftp_directory]
+    ftp.binary = true
+
+    Dir.glob("#{config[:directory_name]}/*") do |file|
+      next if File.extname(file) == '.md5' || File.directory?(file)
+
+      file_name = File.basename(file)
+      file_size = File.size(file)
+
+      if ftp.ls(file_name).empty?
+        critical "FTP file #{file_name} missing"
+      elsif file_size != ftp.size(file_name)
+        critical "FTP file #{file_name} size differ from original"
+      end
+    end
+
+    ok "All files in #{config[:directory_name]} exists on FTP #{config[:ftp_host]}"
+  rescue Net::FTPError => e
+    critical "FTP exception - #{e.message}"
+  ensure
+    ftp&.quit
+  end
+end
---
+++ bin/check-backup-s3.rb
@@ -0,0 +1,74 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'aws-sdk-s3'
+require 'sensu-plugin/check/cli'
+
+class CheckBackupS3 < Sensu::Plugin::Check::CLI
+  option :s3_access_key,
+         short: '-a S3_ACCESS_KEY',
+         long: '--s3-access-key S3_ACCESS_KEY',
+         description: "S3 Access Key. Either set ENV['AWS_ACCESS_KEY'] or provide it as an option",
+         default: ENV['AWS_ACCESS_KEY']
+
+  option :s3_secret_key,
+         short: '-k S3_SECRET_KEY',
+         long: '--s3-secret-key S3_SECRET_KEY',
+         description: "S3 Secret Secret Key. Either set ENV['AWS_SECRET_KEY'] or provide it as an option",
+         default: ENV['AWS_SECRET_KEY']
+
+  option :s3_region,
+         short: '-r S3_REGION',
+         long: '--s3-region REGION',
+         description: 'S3 Region (defaults to eu-west-3).',
+         default: 'eu-west-3'
+
+  option :s3_endpoint,
+         short: '-e S3_ENDPOINT',
+         long: '--s3-endpoint ENDPOINT',
+         description: 'S3 Endpoint (defaults to https://s3.eu-west-3.amazonaws.com).',
+         default: 'https://s3.eu-west-3.amazonaws.com'
+
+  option :bucket_name,
+         short: '-b BUCKET_NAME',
+         long: '--bucket-name',
+         description: 'The name of the S3 bucket where object lives'.dup,
+         required: true
+
+  option :directory_name,
+         short: '-d DIR_NAME',
+         long: '--directory-name',
+         description: 'The name of directory to check',
+         default: '/var/archives'
+
+  def s3_config
+    {
+      access_key_id: config[:s3_access_key],
+      secret_access_key: config[:s3_secret_key],
+      region: config[:s3_region],
+      endpoint: config[:s3_endpoint]
+    }
+  end
+
+  def run
+    s3 = Aws::S3::Client.new(s3_config)
+
+    Dir.glob("#{config[:directory_name]}/*") do |file|
+      next if File.extname(file) == '.md5'
+
+      file_name = File.basename(file)
+      file_size = File.size(file)
+
+      output      = s3.head_object(bucket: config[:bucket_name], key: file_name)
+      remote_size = output[:content_length]
+
+      critical "S3 object #{file_name} size: #{file_size} bytes (bucket #{remote_size})" if file_size != remote_size
+    rescue Aws::S3::Errors::NotFound
+      critical "S3 object #{file_name} not found in bucket #{config[:bucket_name]}"
+    end
+
+    ok "All files in #{config[:directory_name]} exists in S3 bucket #{config[:bucket_name]}"
+  rescue Aws::S3::Errors::ServiceError => e
+    critical "S3 exception - #{e.message} - #{e.backtrace}"
+  end
+end
